6a134c9b42711dc24a8a6a7ddd9147ba790f39f2
Ranya lakoud <lakoud_rania7@outlook.fr>
2021-05-01 09:24:13 +0200

    UI de etre propritaiter bien realiser

--- promoteur_immobiier/lib/constants.dart
+++ promoteur_immobiier/lib/constants.dart
@@ -5,3 +5,5 @@ const kSecondaryColor = Color(0xFF538FAD);
 const ktherdColor = Color(0xFF5F6367);
 const kgmail1 = Color(0xFF4285F4);
 const kgmail2 = Color(0xFFDB4437);
+const Color primaryColor = Color.fromRGBO(17, 82, 253, 1);
+ const Color blackColor = Color.fromRGBO(38, 42, 46, 1);
